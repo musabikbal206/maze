ce0ea6927a5b384cbf67fbb3755829c9485c3543
musabikbal206 <musabikbal206@gmail.com>
2025-12-29 04:52:38 +0300

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'maze-master-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './icon.svg',
+  './manifest.json',
+  // External libraries (Caching these makes it work offline after first load)
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js',
+  'https://unpkg.com/@phosphor-icons/web'
+];
+
+// Install Event: Cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate Event: Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Event: Serve from cache, fall back to network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
